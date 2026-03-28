8db05b7922ac7cc5671500efc2cf0c80fc8baa4a
yoyou458 <yousefeltahawyashraf@gmail.com>
2026-03-28 14:20:16 -0700

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,8 +1,8 @@
 const songs = [
-    {name:"حلف القمر", file:"songs/half_elamar.mp3", img:"half_elamar.jpg"},
-    {name:"الهوى سلطان", file:"songs/el_hawa_sultan.mp3", img:"el_hawa_sultan.jpg"},
-    {name:"ما تقوله ليه", file:"songs/ma_t2olou_leh.mp3", img:"ma_t2olou_leh.jpg"},
-    {name:"كلام الناس", file:"songs/kalam_el_nas.mp3", img:"kalam_el_nas.jpg"},
-    {name:"خسرت كل الناس", file:"songs/khesert_kol_el_nas.mp3", img:"khesert_kol_el_nas.jpg"},
-    {name:"اسمعيلي كلمة", file:"songs/isma3i_kelma.mp3", img:"isma3i_kelma.jpg"} // 
+  {name:"الهوا سلطان", file:"https://drive.google.com/uc?export=download&id=10WRUk9mP928voiV9sQe_5wty3kbdsaS5", img:"halawa_sultan.jpg"},
+  {name:"حلف القمر", file:"https://drive.google.com/uc?export=download&id=1bHzjxHpKYaRVdmXB0fCFS_yxJztybD8c", img:"half_elamar.jpg"},
+  {name:"اسمعيني بكلمه", file:"https://drive.google.com/uc?export=download&id=1jNpjC4_jjUoDwWsZyKdbe0Wvrl4SkgVs", img:"isma3iny_bkalma.jpg"},
+  {name:"كلام الناس", file:"https://drive.google.com/uc?export=download&id=1ibnpi8RoNexDgHLlQdYrKB96pwUavOl1", img:"kalam_elnas.jpg"},
+  {name:"خسرت كل الناس", file:"https://drive.google.com/uc?export=download&id=1nnuebxIqxQ56tduyitgA5qAc-8YGlJD9", img:"khasart_kol_elnas.jpg"},
+  {name:"ما تقوله ليه", file:"https://drive.google.com/uc?export=download&id=1mUjEgZYOgpcJybNsdE3_prueou6DVPm2", img:"ma_te2olo_leh.jpg"}
 ];
